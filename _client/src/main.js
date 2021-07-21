d86669463bc460955edf52d5ee880e2cefda0f41
Spenser Wellen <spenserwellen@gmail.com>
2021-07-20 22:27:02 -0500

    fixed up the github

--- _client/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import Toasted from 'vue-toasted';
-
-createApp(App)
-  .use(router, Toasted, { iconPack: 'fontawesome' })
-  .mount('#app');
